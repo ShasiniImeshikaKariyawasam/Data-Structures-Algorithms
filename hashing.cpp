ece051962eb9ea8237c934a1a4ec93488dbb04c0
ShasiniImeshikaKariyawasam <36620375+ShasiniImeshikaKariyawasam@users.noreply.github.com>
2020-09-01 21:39:56 +0530

    Add files via upload

---
+++ hashing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+void insert(int arr[], int size, int tsize)
+{
+	int pos, element;
+	int n=0;
+	
+	cout<<"Enter the element to be inserted";
+	cin>>element;
+}
+
